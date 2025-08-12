9504237a000cda71331cbf87388a7a3a73116cbb
Ishe <44573071+innocent-george-mithu-nyamusa@users.noreply.github.com>
2025-08-12 20:42:34 +0200

    added privacy policy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <title>Animate Web Application</title>
+      
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
